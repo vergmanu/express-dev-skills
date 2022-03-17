d5c8e7be326197e0d211f7039038f3be4232e76c
vergmanu <vergmanu@gmail.com>
2022-03-16 21:04:00 -0700

    day 2 lab

--- routes/skills.js
+++ routes/skills.js
@@ -7,6 +7,9 @@ const skillsCtrl = require('../controllers/skills')
 /* GET users listing. */
  // All actual paths start with "/todos"
  router.get('/', skillsCtrl.index);
+ router.get('/new', skillsCtrl.new);
  router.get('/:id', skillsCtrl.show);
+ router.post('/', skillsCtrl.create);
+ router.delete('/:id', skillsCtrl.delete);
 
 module.exports = router;
